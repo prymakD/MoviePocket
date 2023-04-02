7a72ae8fc17bcde19cd5d427204920844e449c04
Danila <danpry@st.amu.edu.pl>
2023-04-02 20:32:00 +0200

    Srping boot initialzr + mysql connect(uam server) + thymeleaf(temporarly fo view part) index page bootstraped

---
+++ src/main/java/com/moviePocket/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.moviePocket.repository;
+
+import com.moviePocket.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findById(String username);
+}
