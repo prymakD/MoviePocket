3bf98c5a12fe5cb9016e4e331ffc464bf3bfadc9
Danila <danpry@st.amu.edu.pl>
2023-04-04 17:56:43 +0200

    - added roles functionallity(with addit table many-to-many for Role and User tables)
    - changing SecurityConfiguration and UserServiceImpl classes to avoid cyclic bean problem in security class
    
    Completed v.1 demo Logout/Login module with passwrd encryption and storing users in mysql db
    
    MOV-24 close #1

--- src/main/java/com/moviePocket/Service/UserServiceImpl.java
+++ src/main/java/com/moviePocket/Service/UserServiceImpl.java
@@ -1,43 +1,66 @@
 package com.moviePocket.Service;
 
 import com.moviePocket.controller.dto.UserRegistrationDto;
+import com.moviePocket.entities.Role;
 import com.moviePocket.entities.User;
+import com.moviePocket.repository.RoleRepository;
 import com.moviePocket.repository.UserRepository;
-import org.springframework.security.core.authority.AuthorityUtils;
+import com.moviePocket.util.TbConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    @Autowired
     private UserRepository userRepository;
 
-    private BCryptPasswordEncoder passwordEncoder;
+    @Autowired
+    private RoleRepository roleRepository;
 
-    public UserServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = new BCryptPasswordEncoder();
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+
+    @Override
+    public void save(UserRegistrationDto userDto) {
+        Role role = roleRepository.findByName(TbConstants.Roles.USER);
+
+        if (role == null)
+            role = roleRepository.save(new Role(TbConstants.Roles.USER));
+
+        User user = new User(userDto.getUsername(), userDto.getEmail(), passwordEncoder.encode(userDto.getPassword()),
+                Arrays.asList(role));
+        userRepository.save(user);
     }
 
 
-    @Override
-    public User save(UserRegistrationDto registrationDto) {
-        User user = new User(registrationDto.getUsername(),
-                passwordEncoder.encode(registrationDto.getPassword()),
-                registrationDto.getEmail(), "USER");
-
-        return userRepository.save(user);
-    }
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(username);
-        if (user == null) {
-            throw new UsernameNotFoundException("Invalid username or password.");
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(usernameOrEmail);
+        if (user != null) {
+            return new org.springframework.security.core.userdetails.User(user.getEmail()
+                    , user.getPassword(),
+                    user.getRoles().stream()
+                            .map((role) -> new SimpleGrantedAuthority(role.getName()))
+                            .collect(Collectors.toList()));
+        } else {
+            throw new UsernameNotFoundException("Invalid email or password");
         }
-        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
-                AuthorityUtils.createAuthorityList(user.getRole()));
+    }
+
+    @Override
+    public User findUserByEmail(String email) {
+        return userRepository.findByEmail(email);
     }
 }
