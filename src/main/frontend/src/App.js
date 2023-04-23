358da966979d70921df57116358b19924d60f24b
HelQ <ekinto88@gmail.com>
2023-04-23 17:50:02 +0200

    Add dependencies. Add some components. Add main page with carousel of films from TMDB.

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios';
+import {useEffect, useState} from 'react';
+import Layout from './components/Layout';
+import {Routes, Route} from 'react-router-dom';
+import Home from './components/home/Home';
 function App() {
-  return (
+    const [movies, setMovies] = useState([]);
+    const getMovies = async() =>{
+        try {
+            const response = await axios.get('https://api.themoviedb.org/3/list/1?api_key=1da35d58fd12497b111e4dd1c4a4c004&language=en-US');
+            console.log(response.data.items[0])
+            setMovies(response.data.items)
+        } catch(err){
+            console.log(err);
+        }
+    }
+
+    useEffect(()=> {
+        getMovies();
+    },[])
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Routes>
+            <Route path='/' element={<Layout/>}>
+                <Route path='/' element={<Home movies={movies}/>}></Route>
+            </Route>
+        </Routes>
     </div>
-  );
+    );
 }
 
 export default App;
