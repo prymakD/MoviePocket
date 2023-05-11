1b6e5c0ffcd539bd8f32b490b3438f1afbaf66af
HelQ <ekinto88@gmail.com>
2023-05-11 18:24:09 +0200

    Styling main page. Change a little bit architecture for front-end.

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Registration from "./Registration";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +11,7 @@ root.render(
       <BrowserRouter>
           <Routes>
               <Route path='/*' element={<App />}/>
+              <Route path='/registration' element={<Registration />}/>
           </Routes>
       </BrowserRouter>
   </React.StrictMode>
