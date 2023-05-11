5b464a5c3b391883b7656c42b082ee0a2adf039c
HelQ <ekinto88@gmail.com>
2023-05-11 18:24:09 +0200

    Styling main page. Change a little bit architecture for front-end.

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,35 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
-import {useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Layout from './components/Layout';
 import {Routes, Route} from 'react-router-dom';
 import Home from './components/home/Home';
+import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
     const [movies, setMovies] = useState([]);
-    const getMovies = async() =>{
+    const getMovies = async () => {
         try {
             const response = await axios.get('https://api.themoviedb.org/3/list/1?api_key=1da35d58fd12497b111e4dd1c4a4c004&language=en-US');
             console.log(response.data.items[0])
             setMovies(response.data.items)
-        } catch(err){
+        } catch (err) {
             console.log(err);
         }
     }
 
-    useEffect(()=> {
+    useEffect(() => {
         getMovies();
-    },[])
+    }, [])
 
     return (
-    <div className="App">
-        <Routes>
-            <Route path='/' element={<Layout/>}>
-                <Route path='/' element={<Home movies={movies}/>}></Route>
-            </Route>
-        </Routes>
-    </div>
+        <div className="App">
+            <Routes>
+                <Route path='/' element={<Layout/>}>
+                    <Route path='/' element={<Home movies={movies}/>}></Route>
+                </Route>
+            </Routes>
+        </div>
     );
 }
-
 export default App;
--- src/main/frontend/src/components/Layout.js
+++ src/main/frontend/src/components/Layout.js
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 
 import React from 'react'
+import Header from "./header/Header";
 
 const Layout = () => {
     return(
         <main>
+            <Header/>
             <Outlet/>
         </main>
     )
