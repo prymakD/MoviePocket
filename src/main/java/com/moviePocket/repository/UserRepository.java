7c651e6da61e41dc2a18f443a848030dbf8c131c
pozny <antpaz@st.amu.edu.pl>
2023-05-15 20:23:52 +0200

    email verification

--- src/main/java/com/moviePocket/repository/UserRepository.java
+++ src/main/java/com/moviePocket/repository/UserRepository.java
@@ -10,4 +10,6 @@ import java.util.List;
 public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findById(String username);
     User findByEmail(String mail);
+
+    User findByActivationCode(String code);
 }
