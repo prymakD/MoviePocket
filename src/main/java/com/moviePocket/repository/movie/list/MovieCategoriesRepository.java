0c3c9ec4f60624844decac15a97c6b9104d8e988
pozny <antpaz@st.amu.edu.pl>
2023-06-05 23:32:45 +0200

    fix delete MovieList

--- src/main/java/com/moviePocket/repository/movie/list/MovieCategoriesRepository.java
+++ src/main/java/com/moviePocket/repository/movie/list/MovieCategoriesRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
+@Transactional
 @Repository
 public interface MovieCategoriesRepository extends JpaRepository<MovieCategories, Long> {
 
--- src/main/java/com/moviePocket/repository/movie/list/MovieListRepository.java
+++ src/main/java/com/moviePocket/repository/movie/list/MovieListRepository.java
@@ -5,8 +5,10 @@ import com.moviePocket.entities.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
+@Transactional
 @Repository
 public interface MovieListRepository extends JpaRepository<MovieList, Long> {
 
