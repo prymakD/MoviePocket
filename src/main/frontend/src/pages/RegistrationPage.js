5d202adb5c8e6a7bb1f4878493cb53325ecb14ee
HelQ <ekinto88@gmail.com>
2023-06-06 21:07:47 +0200

    Registration page

---
+++ src/main/frontend/src/pages/RegistrationPage.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './RegistrationPage.css';
+
+const RegistrationPage = () => {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value);
+    };
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    };
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await axios.get('http://localhost:8080/user/f', {
+            });
+
+            console.log('Registration successful!', response.data);
+        } catch (error) {
+            console.error('Error occurred during registration:', error);
+        }
+    };
+
+    return (
+        <div className="registration-container">
+            <h1>Registration Page</h1>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="username">Username:</label>
+                    <input
+                        type="text"
+                        id="username"
+                        value={username}
+                        onChange={handleUsernameChange}
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={handleEmailChange}
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={handlePasswordChange}
+                    />
+                </div>
+                <button type="submit">Register</button>
+            </form>
+        </div>
+    );
+};
+
+export default RegistrationPage;
