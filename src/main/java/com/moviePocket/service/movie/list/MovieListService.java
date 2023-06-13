d2c270f8406cd15afc2050558b9234e28770f792
AntonPazniak <poznyak969@gmail.com>
2023-06-13 14:56:43 +0200

    add ResponseEntity<>

--- src/main/java/com/moviePocket/service/movie/list/MovieListService.java
+++ src/main/java/com/moviePocket/service/movie/list/MovieListService.java
@@ -1,27 +1,28 @@
 package com.moviePocket.service.movie.list;
 
 import com.moviePocket.entities.movie.list.ParsMovieList;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 public interface MovieListService {
 
-    void setMovieList(String email, String title, String content);
+    ResponseEntity<Void> setMovieList(String email, String title, String content);
 
-    void updateMovieListTitle(String email, Long idMovieList, String title);
+    ResponseEntity<Void> updateMovieListTitle(String email, Long idMovieList, String title);
 
-    void updateMovieListContent(String email, Long idMovieList, String content);
+    ResponseEntity<Void> updateMovieListContent(String email, Long idMovieList, String content);
 
-    void deleteMovieList(String email, Long idMovieList);
+    ResponseEntity<Void> deleteMovieList(String email, Long idMovieList);
 
-    List<ParsMovieList> getAllByTitle(String title);
+    ResponseEntity<List<ParsMovieList>> getAllByTitle(String title);
 
-    List<ParsMovieList> getMovieList(Long idMovieList);
+    ResponseEntity<List<ParsMovieList>> getMovieList(Long idMovieList);
 
-    List<ParsMovieList> getAllList();
+    ResponseEntity<List<ParsMovieList>> getAllList();
 
-    List<ParsMovieList> getAllMyList(String email);
+    ResponseEntity<List<ParsMovieList>> getAllMyList(String email);
 
-    List<ParsMovieList> getAllByUsernameList(String username);
+    ResponseEntity<List<ParsMovieList>> getAllByUsernameList(String username);
 
 }
