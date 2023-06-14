479ca6ac6372560f4816e570bf6feb22e42f6f45
pozny <antpaz@st.amu.edu.pl>
2023-06-14 12:24:06 +0200

    minor fix

---
+++ src/main/frontend/src/components/navbar/LogoBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './LogoBar.css';
+
+const LogoBar = () => {
+    return (
+        <div className="Logo">
+            <img
+                src="https://raw.githubusercontent.com/prymakD/MoviePocket/a328870a5299706d0a38d83ace25c757723c276b/src/main/frontend/src/images/logo.png"
+                alt="Logo"
+            />
+        </div>
+    );
+}
+
+export default LogoBar;
--- src/main/frontend/src/components/navbar/Navbar.js
+++ src/main/frontend/src/components/navbar/Navbar.js
@@ -1,12 +1,15 @@
 import './Navbar.css'
 import Navlist from "./Navlist";
 import Userbar from "./Userbar";
+import LogoBar from "./LogoBar";
+
 const Navbar = () => {
 
     return (
         <nav className="Navbar">
             <div className="Container">
                 <div className="NavRow">
+                    <LogoBar/>
                     <a href="/" className="NavBrand">
                         <strong>MoviePocket</strong>
                     </a>
