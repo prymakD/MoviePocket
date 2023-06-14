1c4b984a4c45fda203f4a8336290307644149a65
AntonPazniak <poznyak969@gmail.com>
2023-06-14 12:24:06 +0200

    minor fix

--- src/main/frontend/src/components/navbar/Navlist.js
+++ src/main/frontend/src/components/navbar/Navlist.js
@@ -1,12 +1,15 @@
 import './Navlist.css'
 import {Link} from "react-router-dom";
+
 const Navlist = () => {
 
     return (
         <ul className="Navlist">
             <div className="Search">
                 <input type="text" className="Round"/>
-                <input type="image" className="Submit" src="https://cdn-icons-png.flaticon.com/512/751/751463.png" alt="+"/>
+                <input type="image" className="Submit"
+                       src="https://raw.githubusercontent.com/prymakD/MoviePocket/1458e6c307d0ae5d381bd8607aa7758ccef1a575/src/main/frontend/src/images/search.png"
+                       alt="+"/>
             </div>
             <li className="NavListItem"><Link to="/login" className="NavListLink">SIGN IN</Link></li>
             <li className="NavListItem"><Link to="/registration" className="NavListLink">CREATE ACCOUNT</Link></li>
