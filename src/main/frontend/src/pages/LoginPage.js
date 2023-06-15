4ac81e099cd1caaf5b2fe0d1543c9bcd36c9b2c6
Danila <danpry@st.amu.edu.pl>
2023-06-15 20:08:44 +0200

    added background to the login and RegistrationPage

--- src/main/frontend/src/pages/LoginPage.js
+++ src/main/frontend/src/pages/LoginPage.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import './LoginPage.css';
-import queryString from "query-string";
+import queryString from 'query-string';
 
 const LoginPage = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-
+    const [backgroundImage, setBackgroundImage] = useState('');
 
     const handleEmailChange = (event) => {
         setEmail(event.target.value);
@@ -24,6 +24,7 @@ const LoginPage = () => {
             console.error('Error occurred during logout:', error);
         }
     };
+
     const handleSubmit = async (event) => {
         event.preventDefault();
 
@@ -31,51 +32,89 @@ const LoginPage = () => {
             const params = {
                 username: email,
                 password: password,
-            }
-            const response = await axios.post('http://localhost:8080/login',
+            };
+            const response = await axios.post(
+                'http://localhost:8080/login',
                 queryString.stringify(params),
-                {withCredentials: true});
+                {withCredentials: true}
+            );
 
             console.log('Login successful!', response.data);
 
             const authToken = response.headers['set-cookie'];
 
-            // Сохранение куки на клиенте
+            // Save cookie on the client
             document.cookie = `authToken=${authToken.join(';')}`;
         } catch (error) {
             console.error('Error occurred during login:', error);
         }
     };
 
+    const options = {
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZGEzNWQ1OGZkMTI0OTdiMTExZTRkZDFjNGE0YzAwNCIsInN1YiI6IjY0NDUyZGMwNjUxZmNmMDYxNzliZmY5YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.expCnsMxBP9wfZab438BOkfl0VPQJftRFG7WPkSRyD0'
+        }
+    };
+
+    useEffect(() => {
+        const fetchRandomMovieImage = async () => {
+            try {
+                const response = await axios.get(
+                    'https://api.themoviedb.org/3/movie/popular',
+                    options
+                );
+
+                const randomIndex = Math.floor(
+                    Math.random() * response.data.results.length
+                );
+                const movie = response.data.results[randomIndex];
+
+                setBackgroundImage(`https://image.tmdb.org/t/p/original${movie.backdrop_path}`);
+            } catch (error) {
+                console.error('Error occurred while fetching movie image:', error);
+            }
+        };
+
+        fetchRandomMovieImage();
+    }, []);
+
+
     return (
-        <div className="registration-container">
-            <h1 className="head">SIGN IN</h1>
-            <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <label htmlFor="email">Email</label>
-                    <br/>
-                    <input
-                        className="registration-input"
-                        type="email"
-                        id="email"
-                        value={email}
-                        onChange={handleEmailChange}
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="password">Password</label>
-                    <br/>
-                    <input
-                        className="registration-input"
-                        type="password"
-                        id="password"
-                        value={password}
-                        onChange={handlePasswordChange}
-                    />
-                </div>
-                <button type="submit">Sign in</button>
-                <button onClick={handleLogout}>Logout</button>
-            </form>
+        <div
+            className="image-container"
+            style={{backgroundImage: `url(${backgroundImage})`}}
+        >
+            <div className="registration-container"
+            >
+                <h1 className="head">SIGN IN</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <br/>
+                        <input
+                            className="registration-input"
+                            type="email"
+                            id="email"
+                            value={email}
+                            onChange={handleEmailChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <br/>
+                        <input
+                            className="registration-input"
+                            type="password"
+                            id="password"
+                            value={password}
+                            onChange={handlePasswordChange}
+                        />
+                    </div>
+                    <button type="submit">Sign in</button>
+                    <button onClick={handleLogout}>Logout</button>
+                </form>
+            </div>
         </div>
     );
 };
--- src/main/frontend/src/pages/RegistrationPage.js
+++ src/main/frontend/src/pages/RegistrationPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import './RegistrationPage.css';
 import queryString from "query-string";
@@ -7,6 +7,8 @@ const RegistrationPage = () => {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [backgroundImage, setBackgroundImage] = useState('');
+
 
     const handleUsernameChange = (event) => {
         setUsername(event.target.value);
@@ -37,45 +39,79 @@ const RegistrationPage = () => {
         }
     };
 
+    const options = {
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZGEzNWQ1OGZkMTI0OTdiMTExZTRkZDFjNGE0YzAwNCIsInN1YiI6IjY0NDUyZGMwNjUxZmNmMDYxNzliZmY5YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.expCnsMxBP9wfZab438BOkfl0VPQJftRFG7WPkSRyD0'
+        }
+    };
+
+    useEffect(() => {
+        const fetchRandomMovieImage = async () => {
+            try {
+                const response = await axios.get(
+                    'https://api.themoviedb.org/3/movie/popular',
+                    options
+                );
+
+                const randomIndex = Math.floor(
+                    Math.random() * response.data.results.length
+                );
+                const movie = response.data.results[randomIndex];
+
+                setBackgroundImage(`https://image.tmdb.org/t/p/original${movie.backdrop_path}`);
+            } catch (error) {
+                console.error('Error occurred while fetching movie image:', error);
+            }
+        };
+
+        fetchRandomMovieImage();
+    }, []);
+
     return (
-        <div className="registration-container">
-            <h1 className="head">CREATE ACCOUNT</h1>
-            <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <label htmlFor="username">Username</label>
-                    <br/>
-                    <input
-                        className="registration-input"
-                        type="text"
-                        id="username"
-                        value={username}
-                        onChange={handleUsernameChange}
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="email">Email</label>
-                    <br/>
-                    <input
-                        className="registration-input"
-                        type="email"
-                        id="email"
-                        value={email}
-                        onChange={handleEmailChange}
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="password">Password</label>
-                    <br/>
-                    <input
-                        className="registration-input"
-                        type="password"
-                        id="password"
-                        value={password}
-                        onChange={handlePasswordChange}
-                    />
-                </div>
-                <button type="submit">Join Us</button>
-            </form>
+        <div
+            className="image-container"
+            style={{backgroundImage: `url(${backgroundImage})`}}
+        >
+            <div className="registration-container">
+                <h1 className="head">CREATE ACCOUNT</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="username">Username</label>
+                        <br/>
+                        <input
+                            className="registration-input"
+                            type="text"
+                            id="username"
+                            value={username}
+                            onChange={handleUsernameChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <br/>
+                        <input
+                            className="registration-input"
+                            type="email"
+                            id="email"
+                            value={email}
+                            onChange={handleEmailChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <br/>
+                        <input
+                            className="registration-input"
+                            type="password"
+                            id="password"
+                            value={password}
+                            onChange={handlePasswordChange}
+                        />
+                    </div>
+                    <button type="submit">Join Us</button>
+                </form>
+            </div>
         </div>
     );
 };
