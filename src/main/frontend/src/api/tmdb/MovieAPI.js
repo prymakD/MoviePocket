74022fc9a86ec5d22781172aacfab290b4609da3
HelQ <ekinto88@gmail.com>
2023-06-16 21:32:00 +0200

    Home upcoming movies

--- src/main/frontend/src/api/tmdb/MovieAPI.js
+++ src/main/frontend/src/api/tmdb/MovieAPI.js
@@ -96,3 +96,23 @@ export const getMovieTrailer = async (id) => {
         console.log(err);
     }
 };
+
+export const getUpComingMovies = async () => {
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization:
+                'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZGEzNWQ1OGZkMTI0OTdiMTExZTRkZDFjNGE0YzAwNCIsInN1YiI6IjY0NDUyZGMwNjUxZmNmMDYxNzliZmY5YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.expCnsMxBP9wfZab438BOkfl0VPQJftRFG7WPkSRyD0',
+        },
+    };
+    try {
+        const response = await axios.get(
+            'https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1',
+            options
+        );
+        return response.data.results
+    } catch (err) {
+        console.log(err);
+    }
+}
