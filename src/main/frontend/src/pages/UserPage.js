79547ed1ef836705e70d170333efd9ebc6e642f6
HelQ <ekinto88@gmail.com>
2023-06-17 19:45:45 +0200

    backImage

--- src/main/frontend/src/pages/UserPage.js
+++ src/main/frontend/src/pages/UserPage.js
@@ -1,12 +1,15 @@
 import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import {getUser} from "../api/server/UserAPI";
+import {getRandomMovie} from "../api/tmdb/MovieAPI";
 
 
 const UserPage = () => {
     const {username} = useParams();
 
     const [user, setUser] = useState();
+    const [backgroundImage, setBackgroundImage] = useState('');
+
     const getUserInfo = async () => {
         try {
             const response = await getUser(username);
@@ -16,8 +19,18 @@ const UserPage = () => {
         }
     };
 
+    const getRandomMovieImage = async () => {
+        try {
+            const response = await getRandomMovie();
+            setBackgroundImage(`https://image.tmdb.org/t/p/original${response.backdrop_path}`);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     useEffect(() => {
         getUserInfo().then()
+        getRandomMovieImage().then()
     }, [])
 
     if (!user) {
@@ -26,7 +39,8 @@ const UserPage = () => {
     }
 
     return (
-        <div>
+        <div className="user-page-container"
+             style={{backgroundImage: `url(${backgroundImage})`}}>
             <h1>{user.username}</h1>
             <p>Bio: {user.bio}</p>
             <p>Created: {user.created}</p>
