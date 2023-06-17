4afc51f2fcebb5cb133bac30a775dcabd8a92c58
pozny <antpaz@st.amu.edu.pl>
2023-06-17 16:43:08 +0200

    design fix

--- src/main/frontend/src/components/navbar/Userbar.js
+++ src/main/frontend/src/components/navbar/Userbar.js
@@ -19,7 +19,9 @@ const Userbar= () => {
         (isLoggedIn &&
         <div className='User'>
             <Link to="/settings">
-                <img src="https://cdn.icon-icons.com/icons2/1154/PNG/512/1486564400-account_81513.png" alt='USER'/>
+                <img
+                    src="https://github.com/prymakD/MoviePocket/raw/d36f4f403ed1c15c50b097c93056bbabad50aa87/src/main/frontend/src/images/user.png"
+                    alt='USER'/>
             </Link>
         </div>
         )
