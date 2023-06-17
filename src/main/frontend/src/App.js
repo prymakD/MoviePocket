d0bc324ee25935d4c749c26371c8db20dfef610f
HelQ <ekinto88@gmail.com>
2023-06-17 18:48:10 +0200

    Api rework

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -10,10 +10,12 @@ import SettingsPage from "./pages/SettingsPage";
 import FilmsBrowsingPage from "./pages/FilmsBrowsingPage";
 import LoginPage from "./pages/LoginPage";
 import LostPasswordPage from "./pages/LostPasswordPage"
-import {useEffect, useState} from "react";
+import {createContext, useEffect, useState} from "react";
 import {checkAuth} from "./api/server/UserAPI";
 import UserPage from "./pages/UserPage";
 
+
+export const AuthContext = createContext(null)
 const App = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(null);
 
@@ -33,62 +35,64 @@ const App = () => {
 
     return (
         <div className="App">
-            <Routes>
-                <Route path='/' element={<Layout isLogged={isLoggedIn}/>}>
-                    <Route index element={<Home />}></Route>
-                    {/* For not logged in */}
-                    <Route
-                        path='/registration'
-                        element={
-                            isLoggedIn ?
-                                (<Navigate to="/" replace/>)
-                                :
-                                (<RegistrationPage />)
-                            }
-                    />
-                    <Route
-                        path='/login'
-                        element={
-                            isLoggedIn ?
-                                (<Navigate to="/" replace/>)
-                                :
-                                (<LoginPage />)
-                            }
-                    />
-                    {/* For logged in */}
-                    <Route
-                        path='/forgotPassword'
-                        element={
-                            !isLoggedIn ?
-                                (<Navigate to="/" replace/>)
-                                :
-                                (<LostPasswordPage />)
-                            }
-                    />
-                    <Route
-                        path='/settings'
-                        element={
-                            !isLoggedIn ?
-                                (<Navigate to="/" replace/>)
-                                :
-                                (<SettingsPage />)
-                            }
-                    />
-                    {/* For everyone */}
-                    <Route
-                        path='films/:currentPage'
-                        element={<FilmsBrowsingPage />}
-                    />
-                    <Route
-                        path='film/:id'
-                        element={<FilmPage />}
-                    />
-                    <Route
-                        path='user/:username'
-                        element={<UserPage />}
-                    />
-                </Route>
-            </Routes>
+            <AuthContext.Provider value={isLoggedIn}>
+                <Routes>
+                    <Route path='/' element={<Layout isLogged={isLoggedIn}/>}>
+                        <Route index element={<Home />}></Route>
+                        {/* For not logged in */}
+                        <Route
+                            path='/registration'
+                            element={
+                                isLoggedIn ?
+                                    (<Navigate to="/" replace/>)
+                                    :
+                                    (<RegistrationPage />)
+                                }
+                        />
+                        <Route
+                            path='/login'
+                            element={
+                                isLoggedIn ?
+                                    (<Navigate to="/" replace/>)
+                                    :
+                                    (<LoginPage />)
+                                }
+                        />
+                        {/* For logged in */}
+                        <Route
+                            path='/forgotPassword'
+                            element={
+                                !isLoggedIn ?
+                                    (<Navigate to="/" replace/>)
+                                    :
+                                    (<LostPasswordPage />)
+                                }
+                        />
+                        <Route
+                            path='/settings'
+                            element={
+                                !isLoggedIn ?
+                                    (<Navigate to="/" replace/>)
+                                    :
+                                    (<SettingsPage />)
+                                }
+                        />
+                        {/* For everyone */}
+                        <Route
+                            path='films/:currentPage'
+                            element={<FilmsBrowsingPage />}
+                        />
+                        <Route
+                            path='film/:id'
+                            element={<FilmPage />}
+                        />
+                        <Route
+                            path='user/:username'
+                            element={<UserPage />}
+                        />
+                    </Route>
+                </Routes>
+            </AuthContext.Provider>
         </div>
     );
 }
