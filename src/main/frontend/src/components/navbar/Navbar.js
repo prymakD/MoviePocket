e1c95076ac790612ee86010914f8b46a16267f52
HelQ <ekinto88@gmail.com>
2023-06-17 19:45:45 +0200

    backImage

--- src/main/frontend/src/components/navbar/Navbar.js
+++ src/main/frontend/src/components/navbar/Navbar.js
@@ -3,32 +3,12 @@ import Navlist from "./Navlist";
 import Userbar from "./Userbar";
 import LogoBar from "./LogoBar";
 import {Link} from "react-router-dom";
-import {useEffect, useState} from "react";
-import {checkAuth, getUsernameByAuth} from "../../api/server/UserAPI";
+import {useContext, useState} from "react";
+import {AuthContext} from "../../App";
 
 const Navbar = () => {
-    const [isLoggedIn, setIsLoggedIn] = useState(null);
     const [userName, setUserName] = useState('');
-
-    const getUserName = async () => {
-        try {
-            const response = await getUsernameByAuth();
-            setUserName(response);
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    const authenticate = async () => {
-        const isAuthenticated = await checkAuth();
-        setIsLoggedIn(isAuthenticated);
-        await getUserName();
-    };
-
-    useEffect(() => {
-        authenticate().then();
-    }, []);
-
+    const isLoggedIn = useContext(AuthContext);
     return (
         <nav className="Navbar">
             <div className="Container">
