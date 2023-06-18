f30efaa2069cf7134cf3708299ace9f69ac42dce
HelQ <ekinto88@gmail.com>
2023-06-19 00:22:54 +0200

    Components reload when delete

--- src/main/frontend/src/pages/FilmPage.js
+++ src/main/frontend/src/pages/FilmPage.js
@@ -132,6 +132,7 @@ const FilmPage = () => {
                     {reviews.map((review) => (
                         <SingleReview
                             review={review}
+                            updateReviews={getReviews}
                         />
                     ))}
                 </div>
