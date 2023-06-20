5fd87520b4da9df77d673b8f056ba22eb85e9b06
Danila <danpry@st.amu.edu.pl>
2023-06-20 05:09:59 +0200

    minor fix: DislikedMovieButton.js fix

--- src/main/frontend/src/components/buttons/DislikedMovieButton.js
+++ src/main/frontend/src/components/buttons/DislikedMovieButton.js
@@ -39,7 +39,7 @@ const DislikedMovieButton = ({idMovie, className}) => {
     };
 
     useEffect(() => {
-        getDislikedMovie().then()
+        getDislikedMovieState().then()
     }, [idMovie]);
 
     return (
